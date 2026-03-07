af395271a1ff4106d1d3e90605d315e70180d626
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-03-07 06:59:07 +0600

    login auth add

--- script/index.js
+++ script/index.js
@@ -1,1 +1,18 @@
-console.log("js");
+const loginBtn = document.getElementById("login-btn");
+
+loginBtn.addEventListener("click", () => {
+  const inputPassword = document.getElementById("input-password");
+  const inputName = document.getElementById("input-name");
+
+  const username = inputName.value;
+  const password = inputPassword.value;
+
+  
+
+  if (username.toLowerCase() === "admin" && password.toLowerCase() === 'admin123') {
+    alert("login successful")
+  } else {
+    alert("Please input valid username and password")
+  }
+
+})
